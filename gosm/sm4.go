a9b04c44dc0d43c0e594be02e23269f0edf87f7f
BeDreamCoder <tailinzhang1993@gmail.com>
2020-05-05 15:29:12 +0800

    add sm4 crypto

--- gosm/sm4.go
+++ gosm/sm4.go
@@ -12,6 +12,6 @@ func (gs *GoSm4) NewSm4Cipher(key []byte) (cipher.Block, error) {
 	return sm4.NewCipher(key)
 }
 
-func (gs *GoSm4) SM4BlockSize() int {
+func (gs *GoSm4) Sm4BlockSize() int {
 	return sm4.BlockSize
 }
